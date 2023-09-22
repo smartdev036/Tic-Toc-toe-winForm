0b972674b6164d0e45ade886030c0e125fd6a3ff
smart <smartdev036@outlook.com>
2023-09-22 08:33:02 +0200

    Finalized building Tik-tok-toe

---
+++ YAhamedAssignment1/YAhamedForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace YAhamedAssignment1
+{
+    partial class YAhamedForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox22 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox02 = new System.Windows.Forms.PictureBox();
+            this.pictureBox21 = new System.Windows.Forms.PictureBox();
+            this.pictureBox20 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox01 = new System.Windows.Forms.PictureBox();
+            this.pictureBox00 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox00)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox22
+            // 
+            this.pictureBox22.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox22.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox22.Location = new System.Drawing.Point(182, 176);
+            this.pictureBox22.Name = "pictureBox22";
+            this.pictureBox22.Size = new System.Drawing.Size(79, 76);
+            this.pictureBox22.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox22.TabIndex = 0;
+            this.pictureBox22.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox12.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox12.Location = new System.Drawing.Point(182, 94);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(79, 76);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox12.TabIndex = 0;
+            this.pictureBox12.TabStop = false;
+            // 
+            // pictureBox02
+            // 
+            this.pictureBox02.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox02.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox02.Location = new System.Drawing.Point(182, 12);
+            this.pictureBox02.Name = "pictureBox02";
+            this.pictureBox02.Size = new System.Drawing.Size(79, 76);
+            this.pictureBox02.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox02.TabIndex = 0;
+            this.pictureBox02.TabStop = false;
+            // 
+            // pictureBox21
+            // 
+            this.pictureBox21.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox21.Location = new System.Drawing.Point(97, 176);
+            this.pictureBox21.Name = "pictureBox21";
+            this.pictureBox21.Size = new System.Drawing.Size(79, 76);
+            this.pictureBox21.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox21.TabIndex = 0;
+            this.pictureBox21.TabStop = false;
+            // 
+            // pictureBox20
+            // 
+            this.pictureBox20.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox20.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox20.Location = new System.Drawing.Point(12, 176);
+            this.pictureBox20.Name = "pictureBox20";
+            this.pictureBox20.Size = new System.Drawing.Size(79, 76);
+            this.pictureBox20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox20.TabIndex = 0;
+            this.pictureBox20.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox11.Location = new System.Drawing.Point(97, 94);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(79, 76);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 0;
+            this.pictureBox11.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox10.Location = new System.Drawing.Point(12, 94);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(79, 76);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 0;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox01
+            // 
+            this.pictureBox01.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox01.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox01.Location = new System.Drawing.Point(97, 12);
+            this.pictureBox01.Name = "pictureBox01";
+            this.pictureBox01.Size = new System.Drawing.Size(79, 76);
+            this.pictureBox01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox01.TabIndex = 0;
+            this.pictureBox01.TabStop = false;
+            // 
+            // pictureBox00
+            // 
+            this.pictureBox00.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox00.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox00.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox00.Name = "pictureBox00";
+            this.pictureBox00.Size = new System.Drawing.Size(79, 76);
+            this.pictureBox00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox00.TabIndex = 0;
+            this.pictureBox00.TabStop = false;
+            // 
+            // YAhamedForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 265);
+            this.Controls.Add(this.pictureBox22);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox02);
+            this.Controls.Add(this.pictureBox21);
+            this.Controls.Add(this.pictureBox20);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox01);
+            this.Controls.Add(this.pictureBox00);
+            this.Name = "YAhamedForm";
+            this.Text = "Tic Toc Toe";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox00)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox00;
+        private System.Windows.Forms.PictureBox pictureBox01;
+        private System.Windows.Forms.PictureBox pictureBox02;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox20;
+        private System.Windows.Forms.PictureBox pictureBox21;
+        private System.Windows.Forms.PictureBox pictureBox22;
+    }
+}
+
---
+++ YAhamedAssignment1/YAhamedForm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace YAhamedAssignment1
+{
+    public partial class YAhamedForm : Form
+    {
+        private char currentPlayer = 'X';
+        private char[,] board = new char[3, 3];
+        private PictureBox[,] pictureBoxes = new PictureBox[3, 3];
+        private const char Empty = ' ';
+
+        public YAhamedForm()
+        {
+            InitializeComponent();
+            InitializePictureBoxes();
+            InitializeGame();
+        }
+
+        private void InitializePictureBoxes()
+        {
+            pictureBoxes[0, 0] = pictureBox00;
+            pictureBoxes[0, 1] = pictureBox01;
+            pictureBoxes[0, 2] = pictureBox02;
+            pictureBoxes[1, 0] = pictureBox10;
+            pictureBoxes[1, 1] = pictureBox11;
+            pictureBoxes[1, 2] = pictureBox12;
+            pictureBoxes[2, 0] = pictureBox20;
+            pictureBoxes[2, 1] = pictureBox21;
+            pictureBoxes[2, 2] = pictureBox22;
+
+            foreach (PictureBox pictureBox in pictureBoxes)
+            {
+                pictureBox.Click += PictureBox_Click;
+            }
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void InitializeGame()
+        {
+            currentPlayer = 'X';
+
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    board[row, col] = Empty;
+                }
+            }
+
+            foreach (PictureBox pictureBox in pictureBoxes)
+            {
+                pictureBox.Image = Properties.Resources.Bg;
+                pictureBox.Enabled = true;
+            }
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            PictureBox pictureBox = (PictureBox)sender;
+            int row = int.Parse(pictureBox.Name.Substring(10, 1));
+            int col = int.Parse(pictureBox.Name.Substring(11, 1));
+
+            if (board[row, col] == Empty)
+            {
+                board[row, col] = currentPlayer;
+                pictureBox.Image = (currentPlayer == 'X') ? Properties.Resources.Cross : Properties.Resources.Ring;
+                pictureBox.Enabled = false;
+
+                if (CheckForWin())
+                {
+                    MessageBox.Show(currentPlayer + " wins!");
+                    InitializeGame();
+                }
+                else if (IsBoardFull())
+                {
+                    MessageBox.Show("Tie!");
+                    InitializeGame();
+                }
+                else
+                {
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                }
+            }
+        }
+
+        private bool CheckForWin()
+        {
+            for (int idx = 0; idx < 3; idx++)
+            {
+                // Check rows
+                if (board[idx, 0] == currentPlayer && board[idx, 1] == currentPlayer && board[idx, 2] == currentPlayer)
+                    return true;
+                // Check columns
+                if (board[0, idx] == currentPlayer && board[1, idx] == currentPlayer && board[2, idx] == currentPlayer)
+                    return true;
+            }
+
+            // Check diagonals for a win
+            if (board[0, 0] == currentPlayer && board[1, 1] == currentPlayer && board[2, 2] == currentPlayer)
+                return true;
+            if (board[0, 2] == currentPlayer && board[1, 1] == currentPlayer && board[2, 0] == currentPlayer)
+                return true;
+
+            return false;
+        }
+
+        private bool IsBoardFull()
+        {
+            foreach (char cell in board)
+            {
+                if (cell == Empty)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
